7e9d12a3c6ca24d5675a6014f8d1b6777661c124
insobl <kapustin.daniil@hexly.ru>
2022-12-08 12:01:00 +0300

    final fix

--- bin/NameGenerator.js
+++ bin/NameGenerator.js
@@ -1,4 +1,5 @@
 #!/usr/bin/env node
-import {names} from '../src/arrayOfNames.js';
-import {randomizerForNames, congratulations} from '../src/index.js';
+import names from '../src/arrayOfNames.js';
+import { randomizerForNames, congratulations } from '../src/index.js';
+
 console.log(congratulations(names, randomizerForNames()));
