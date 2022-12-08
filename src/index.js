089632166db20ffa4d02a6143d9858d583922736
insobl <kapustin.daniil@hexly.ru>
2022-12-08 11:23:52 +0300

    add js supp files

---
+++ src/index.js
@@ -0,0 +1,6 @@
+export function randomizerForNames (){
+return Math.floor(Math.random() * 2737)
+}
+export function congratulations(names, index){
+    return `Congratulations. You get name: ${names[index]}!!!`
+}
