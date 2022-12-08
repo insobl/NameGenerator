d9098829ae1cbb9a74d7423cfb72f1f8e5428878
insobl <kapustin.daniil@hexly.ru>
2022-12-08 11:29:04 +0300

    edit

--- bin/NameGenerator.js
+++ bin/NameGenerator.js
@@ -1,3 +1,4 @@
+#!/usr/bin/env node
 import {names} from '../src/arrayOfNames.js';
 import {randomizerForNames, congratulations} from '../src/index.js';
 console.log(congratulations(names, randomizerForNames()));
